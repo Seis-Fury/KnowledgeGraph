640077cb8daa11cb6be971ceaa94c9a85f4b9b11
Fury <furycavalier235@163.com>
2017-07-26 09:04:54 +0800

    关于FudanNlp的shell接口封装

---
+++ FnlpNER.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+usage() {
+	echo "Usage: $0 filename" >&2
+}
+if [ $# -gt 1 ]; then
+	usage
+fi
+
+ARGS="-keepAllWhitespaces false"
+if [ $# -eq 1 ];then
+	input=$(cat $1)
+fi
+
+BASEDIR=/Users/rich/Arch/fnlp
+JAVACMD="java -Xmx1024m -Dfile.encoding=UTF-8 -classpath ${BASEDIR}/fnlp-core/target/fnlp-core-2.1-SNAPSHOT.jar:libs/trove4j-3.0.3.jar:libs/commons-cli-1.2.jar org.fnlp.nlp.cn.tag.NERTagger -s ${BASEDIR}/models/seg.m ${BASEDIR}/models/pos.m "
+${JAVACMD} "$input" 2>&1
+
